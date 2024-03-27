f0809ce0ec42a6d67035e8939588ca508f5937d4
IjlalWindhi <ijlalwindhi15@gmail.com>
2024-03-27 12:31:40 +0700

    add(firebase):get firestore

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -16,7 +16,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
 
 // Export the firebase app and analytics
-export { app, analytics };
+export { app, db };
