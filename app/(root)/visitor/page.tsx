a02e2a934852531a732c6724071b1cb7d4249a6b
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2024-03-29 01:13:26 +0700

    change(visitors):change font, gap, and adjust time out visitor name

--- app/(root)/visitor/page.tsx
+++ app/(root)/visitor/page.tsx
@@ -1,14 +1,14 @@
-"use client";
-import { useEffect, useState } from "react";
-import { doc, onSnapshot, setDoc } from "firebase/firestore";
-import { db } from "@/lib/firebase";
+'use client';
+import { useEffect, useState } from 'react';
+import { doc, onSnapshot, setDoc } from 'firebase/firestore';
+import { db } from '@/lib/firebase';
 
 const Visitor = () => {
   // state
   const [totalVisitors, setTotalVisitors] = useState<number>(0);
-  const [lastVisitor, setLastVisitor] = useState<string>("-");
+  const [lastVisitor, setLastVisitor] = useState<string>('-');
   const [isReset, setIsReset] = useState<boolean>(false);
-  const visitorRef = doc(db, "preview", "visitors");
+  const visitorRef = doc(db, 'preview', 'visitors');
 
   // method
   async function updateTotalVisitors() {
@@ -28,28 +28,28 @@ const Visitor = () => {
     if (isReset) {
       setTimeout(() => {
         setDoc(visitorRef, {
-          last_visitor: "-", // reset last visitor
+          last_visitor: '-', // reset last visitor
           total_visitors: totalVisitors,
           updated_at: new Date().toLocaleString(),
         });
-      }, 6000);
+      }, 12000);
     }
   }, [isReset, totalVisitors, visitorRef]);
   return (
-    <section className="bg-hero-visitors relative min-h-screen h-full w-full bg-cover bg-no-repeat">
-      <div className="flex flex-col w-full h-full min-h-screen items-center justify-center gap-10 z-50 text-white relative">
+    <section className="bg-hero-visitors relative min-h-screen h-full w-full bg-cover bg-no-repeat bg-center">
+      <div className="flex flex-col w-full h-full min-h-screen items-center justify-center gap-14 z-50 text-white relative">
         <h1 className="font-handwriting text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
           Selamat Datang
         </h1>
-        <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-semibold">
+        <p className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold font-serif">
           {lastVisitor}
         </p>
-        <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
-          Total pengunjung:{" "}
+        <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-serif">
+          Total pengunjung:{' '}
           <span className="font-semibold">{totalVisitors}</span>
         </p>
       </div>
-      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-70 z-10"></div>
+      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-75 z-10"></div>
     </section>
   );
 };
