bf316041607e76a72389a991a19db6a25d440712
IjlalWindhi <ijlalwindhi15@gmail.com>
2024-03-28 16:11:10 +0700

    add(home):backsound music

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen h-full flex-col items-center justify-center gap-4">
+      <audio autoPlay controls>
+        <source src="/music/backsound.mp3" type="audio/mpeg" />
+      </audio>
       <h1>Dalam Pengembangan</h1>
-      <h2 className="p-4 border rounded-lg">Undangan Pernikahan Rizki & Sherly</h2>
+      <h2 className="p-4 border rounded-lg">
+        Undangan Pernikahan Rizki & Sherly
+      </h2>
     </main>
   );
 }
