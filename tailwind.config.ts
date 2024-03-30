2d134f4ca4066a991aa264f54f810921b7bcc9ba
Ijlal Windhi <73152962+ijlalWindhi@users.noreply.github.com>
2024-03-30 23:49:58 +0700

    add(tailwind):custom color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "hero-visitors": "url('/images/bg-visitors.webp')",
+        'hero-visitors': "url('/images/bg-visitors.webp')",
       },
       fontFamily: {
-        handwriting: ["Sacramento", "cursive"],
+        handwriting: ['Sacramento', 'cursive'],
+      },
+      colors: {
+        primary: '#800000',
+        secondary: '#FFFFFF',
       },
     },
   },
