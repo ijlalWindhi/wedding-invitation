745ab9c812617cde780bec2171afb71301d001ab
IjlalWindhi <ijlalwindhi15@gmail.com>
2024-04-01 18:59:33 +0700

    add(contants):option category and sessions

--- constants/index.ts
+++ constants/index.ts
@@ -1,7 +1,49 @@
 export const defaultValues = {
-  name: "",
-  address: "",
-  category: "",
-  session: 1,
-  numberOfVisitor: 1,
+  name: undefined,
+  address: undefined,
+  category: undefined,
+  session: undefined,
+  numberOfVisitor: undefined,
+};
+
+export const sessionOptions = {
+  "Sesi 1": {
+    label: "Sesi 1",
+    value: 1,
+  },
+  "Sesi 2": {
+    label: "Sesi 2",
+    value: 2,
+  },
+};
+
+export const categoryOptions = {
+  "keluarga sherly": {
+    label: "Keluarga Sherly",
+    value: 1,
+  },
+  "keluarga rizki": {
+    label: "Keluarga Rizki",
+    value: 2,
+  },
+  semeru: {
+    label: "Semeru",
+    value: 3,
+  },
+  bhayangkara: {
+    label: "Bhayangkara",
+    value: 4,
+  },
+  kauman: {
+    label: "Kauman",
+    value: 5,
+  },
+  bogo: {
+    label: "Bogo",
+    value: 6,
+  },
+  umum: {
+    label: "Umum",
+    value: 7,
+  },
 };
