222aa4244cebc28165c6d186c4997e393aa72296
IjlalWindhi <ijlalwindhi15@gmail.com>
2024-04-02 19:09:19 +0700

    change(visitor);handle when no data, and show hide pagination

--- app/(root)/admin/add-visitor/page.tsx
+++ app/(root)/admin/add-visitor/page.tsx
@@ -26,6 +26,7 @@ import { numberPagination } from "@/lib/utils";
 import { useVisitorStore } from "@/store/visitor";
 import Lottie from "react-lottie";
 import Loading from "@/public/animation/loading.json";
+import NoData from "@/public/animation/no_data.json";
 
 const AddVisitor = () => {
   // state & variable
@@ -95,101 +96,120 @@ const AddVisitor = () => {
       <ModalAddVisitor fetchAllVisitors={fetchAllVisitors} />
       <ModalDeleteVisitor fetchAllVisitors={fetchAllVisitors} />
       <div className="flex justify-between gap-4 flex-wrap">
-        <h1 className="font-semibold text-xl">Data Pengunjung Pernikahan</h1>
+        <h1 className="font-semibold text-xl">Data Tamu Pernikahan</h1>
         <Button
           onClick={() => {
             setIsOpenModal(!isOpenModal);
           }}
         >
-          Tambah
+          Tambah Tamu
           <PlusCircle className="ml-2 h-4 w-4" />
         </Button>
       </div>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>No</TableHead>
-            <TableHead className="w-1/4">Nama</TableHead>
-            <TableHead className="w-1/4">Alamat</TableHead>
-            <TableHead className="w-1/6">Kategori</TableHead>
-            <TableHead>Sesi</TableHead>
-            <TableHead>Total Pengunjung</TableHead>
-            <TableHead>Aksi</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {visitors.slice(startIndex, endIndex).map((visitor, index) => (
-            <TableRow key={visitor.uuid}>
-              <TableCell>
-                {numberPagination({ currentPage, itemsPerPage, index })}
-              </TableCell>
-              <TableCell>{visitor.name}</TableCell>
-              <TableCell>{visitor.address}</TableCell>
-              <TableCell>{visitor.category}</TableCell>
-              <TableCell>{visitor.session}</TableCell>
-              <TableCell>{visitor.numberOfVisitor}</TableCell>
-              <TableCell className="flex gap-2">
-                <Button variant="outline" size="icon">
-                  <Info className="h-4 w-4" />
-                </Button>
-                <Button variant="outline" size="icon">
-                  <Edit2
-                    className="h-4 w-4"
-                    onClick={() => {
-                      setIsOpenModal(!isOpenModal);
-                      setSelectedVisitor(visitor);
-                    }}
-                  />
-                </Button>
-                <Button
-                  variant="outline"
-                  size="icon"
-                  onClick={() => {
-                    setIsOpenModalDelete(!isOpenModalDelete);
-                    setSelectedUuidVisitor(visitor.uuid);
-                  }}
-                >
-                  <Trash className="h-4 w-4" />
-                </Button>
-              </TableCell>
+      {visitors.length === 0 ? (
+        <div className="w-full min-h-[85vh] flex items-center md:w-3/4 lg:w-1/2 xl:w-1/4 self-center">
+          <Lottie
+            options={{
+              loop: true,
+              autoplay: true,
+              animationData: NoData,
+            }}
+            isClickToPauseDisabled
+          />
+        </div>
+      ) : (
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>No</TableHead>
+              <TableHead className="w-1/4">Nama</TableHead>
+              <TableHead className="w-1/4 min-w-max">Alamat</TableHead>
+              <TableHead className="w-1/6">Kategori</TableHead>
+              <TableHead>Sesi</TableHead>
+              <TableHead>Total Tamu</TableHead>
+              <TableHead>Aksi</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <Pagination>
-        <PaginationContent>
-          <PaginationItem>
-            <PaginationPrevious
-              onClick={() => {
-                if (currentPage > 1) {
-                  handlePageChange(currentPage - 1);
-                }
-              }}
-            />
-          </PaginationItem>
-          {Array.from({ length: totalPages }, (_, i) => i + 1).map(
-            (pageNumber) => (
-              <PaginationItem key={pageNumber}>
-                <PaginationLink
-                  onClick={() => handlePageChange(pageNumber)}
-                  isActive={pageNumber === currentPage}
-                >
-                  {pageNumber}
-                </PaginationLink>
+          </TableHeader>
+          <TableBody>
+            {visitors.slice(startIndex, endIndex).map((visitor, index) => (
+              <TableRow key={visitor.uuid}>
+                <TableCell>
+                  {numberPagination({ currentPage, itemsPerPage, index })}
+                </TableCell>
+                <TableCell>{visitor.name}</TableCell>
+                <TableCell>{visitor.address}</TableCell>
+                <TableCell>{visitor.category}</TableCell>
+                <TableCell>{visitor.session}</TableCell>
+                <TableCell>{visitor.numberOfVisitor}</TableCell>
+                <TableCell className="flex items-center justify-center gap-2">
+                  <Button variant="outline" size="icon">
+                    <Info className="h-4 w-4" />
+                  </Button>
+                  <Button variant="outline" size="icon">
+                    <Edit2
+                      className="h-4 w-4"
+                      onClick={() => {
+                        setIsOpenModal(!isOpenModal);
+                        setSelectedVisitor(visitor);
+                      }}
+                    />
+                  </Button>
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    onClick={() => {
+                      setIsOpenModalDelete(!isOpenModalDelete);
+                      setSelectedUuidVisitor(visitor.uuid);
+                    }}
+                  >
+                    <Trash className="h-4 w-4" />
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      )}
+      {totalPages !== 0 && (
+        <Pagination>
+          <PaginationContent>
+            {currentPage !== 1 && (
+              <PaginationItem>
+                <PaginationPrevious
+                  onClick={() => {
+                    if (currentPage > 1) {
+                      handlePageChange(currentPage - 1);
+                    }
+                  }}
+                />
               </PaginationItem>
-            )
-          )}
-          <PaginationItem>
-            <PaginationNext
-              onClick={() => {
-                if (currentPage !== totalPages) {
-                  handlePageChange(currentPage + 1);
-                }
-              }}
-            />
-          </PaginationItem>
-        </PaginationContent>
-      </Pagination>
+            )}
+            {Array.from({ length: totalPages }, (_, i) => i + 1).map(
+              (pageNumber) => (
+                <PaginationItem key={pageNumber}>
+                  <PaginationLink
+                    onClick={() => handlePageChange(pageNumber)}
+                    isActive={pageNumber === currentPage}
+                  >
+                    {pageNumber}
+                  </PaginationLink>
+                </PaginationItem>
+              )
+            )}
+            {currentPage !== totalPages && (
+              <PaginationItem>
+                <PaginationNext
+                  onClick={() => {
+                    if (currentPage !== totalPages) {
+                      handlePageChange(currentPage + 1);
+                    }
+                  }}
+                />
+              </PaginationItem>
+            )}
+          </PaginationContent>
+        </Pagination>
+      )}
     </main>
   );
 };
