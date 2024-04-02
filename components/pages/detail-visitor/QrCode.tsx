6a0069e1c10f92e37d655d48cf45c44024c2a39c
IjlalWindhi <ijlalwindhi15@gmail.com>
2024-04-02 22:01:25 +0700

    add(component):QR code component

---
+++ components/pages/detail-visitor/QrCode.tsx
@@ -0,0 +1,47 @@
+import { QRCode } from "react-qrcode-logo";
+import { Download } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+function QrCode({
+  url,
+  nameFile,
+}: Readonly<{ url: string; nameFile: string }>) {
+  // method
+  const downloadQRCode = () => {
+    const canvas = document.getElementById("QR") as HTMLCanvasElement;
+    if (canvas) {
+      const pngUrl = canvas
+        .toDataURL("image/png")
+        .replace("image/png", "image/octet-stream");
+      let downloadLink = document.createElement("a");
+      downloadLink.href = pngUrl;
+      downloadLink.download = `${nameFile}.png`;
+      document.body.appendChild(downloadLink);
+      downloadLink.click();
+      document.body.removeChild(downloadLink);
+    }
+  };
+  return (
+    <div className="flex items-start gap-4">
+      <QRCode
+        value={url}
+        size={230}
+        logoImage="https://res.cloudinary.com/dhhyqadco/image/upload/v1712069743/wedding_invitation_sherly_rizki.jpg"
+        logoHeight={100}
+        logoWidth={100}
+        logoOpacity={1}
+        logoPadding={1}
+        enableCORS={true}
+        qrStyle="squares"
+        eyeRadius={0}
+        id={"QR"}
+      />
+      <Button onClick={downloadQRCode}>
+        <Download className="w-4 h-4 mr-2" />
+        Download
+      </Button>
+    </div>
+  );
+}
+
+export default QrCode;
