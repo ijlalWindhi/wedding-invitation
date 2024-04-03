6c4c64a20936e6d0945c6c33b8c0d3b22997c270
IjlalWindhi <ijlalwindhi15@gmail.com>
2024-04-03 16:07:58 +0700

    add(visitor):sort data by name

--- app/(root)/admin/add-visitor/page.tsx
+++ app/(root)/admin/add-visitor/page.tsx
@@ -196,46 +196,49 @@ const AddVisitor = () => {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {visitors.slice(startIndex, endIndex).map((visitor, index) => (
-              <TableRow key={visitor.uuid}>
-                <TableCell>
-                  {numberPagination({ currentPage, itemsPerPage, index })}
-                </TableCell>
-                <TableCell className="!text-left">{visitor.name}</TableCell>
-                <TableCell>{visitor.address}</TableCell>
-                <TableCell className="min-w-max w-full">
-                  {visitor.category}
-                </TableCell>
-                <TableCell>{visitor.session}</TableCell>
-                <TableCell>{visitor.numberOfVisitor}</TableCell>
-                <TableCell className="flex items-center justify-center gap-2">
-                  <Link href={`/admin/add-visitor/${visitor.uuid}`}>
+            {visitors
+              .slice(startIndex, endIndex)
+              .sort((a, b) => a.name.localeCompare(b.name))
+              .map((visitor, index) => (
+                <TableRow key={visitor.uuid}>
+                  <TableCell>
+                    {numberPagination({ currentPage, itemsPerPage, index })}
+                  </TableCell>
+                  <TableCell className="!text-left">{visitor.name}</TableCell>
+                  <TableCell>{visitor.address}</TableCell>
+                  <TableCell className="min-w-max w-full">
+                    {visitor.category}
+                  </TableCell>
+                  <TableCell>{visitor.session}</TableCell>
+                  <TableCell>{visitor.numberOfVisitor}</TableCell>
+                  <TableCell className="flex items-center justify-center gap-2">
+                    <Link href={`/admin/add-visitor/${visitor.uuid}`}>
+                      <Button variant="outline" size="icon">
+                        <Info className="h-4 w-4" />
+                      </Button>
+                    </Link>
                     <Button variant="outline" size="icon">
-                      <Info className="h-4 w-4" />
+                      <Edit2
+                        className="h-4 w-4"
+                        onClick={() => {
+                          setIsOpenModal(!isOpenModal);
+                          setSelectedVisitor(visitor);
+                        }}
+                      />
                     </Button>
-                  </Link>
-                  <Button variant="outline" size="icon">
-                    <Edit2
-                      className="h-4 w-4"
+                    <Button
+                      variant="outline"
+                      size="icon"
                       onClick={() => {
-                        setIsOpenModal(!isOpenModal);
-                        setSelectedVisitor(visitor);
+                        setIsOpenModalDelete(!isOpenModalDelete);
+                        setSelectedUuidVisitor(visitor.uuid);
                       }}
-                    />
-                  </Button>
-                  <Button
-                    variant="outline"
-                    size="icon"
-                    onClick={() => {
-                      setIsOpenModalDelete(!isOpenModalDelete);
-                      setSelectedUuidVisitor(visitor.uuid);
-                    }}
-                  >
-                    <Trash className="h-4 w-4" />
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))}
+                    >
+                      <Trash className="h-4 w-4" />
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
           </TableBody>
         </Table>
       )}
